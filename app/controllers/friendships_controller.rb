3f1dac79e103b38ff26770758b1fb61f67a1489e
bassant sayed <bassantsayed19@gmail.com>
2019-04-11 15:17:01 +0200

    adding friends filtration

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,30 +1,29 @@
 class FriendshipsController < ApplicationController
 
     def index
-        @users = User.all
+        @users = User.where('id NOT IN (?) AND id != (?)',Friendship.select("friend_id").where(user_id: current_user.try(:id)),current_user.try(:id)) 
     end
 
-
-def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-        redirect_to friendship_path(current_user.try(:id))
-    else
-      flash[:notice] = "Unable to add friend."
-    #   redirect_to root_url
+    def create
+        @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+        if @friendship.save
+            redirect_to friendship_path(current_user.try(:id))
+        else
+          flash[:notice] = "Unable to add friend."
+        #   redirect_to root_url
+        end
     end
-end
 
-def show
-    @user = User.find(params[:id])
-end
+    def show
+        @user = User.find(params[:id])
+    end
   
-  
-def destroy
-    @friendship = current_user.friendships.find(params[:id])
-    @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to friendship_path(current_user.try(:id))
+    
+  def destroy
+      @friendship = current_user.friendships.find(params[:id])
+      @friendship.destroy
+      flash[:notice] = "Removed friendship."
+      redirect_to friendship_path(current_user.try(:id))
   end
   
   
